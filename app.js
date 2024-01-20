cbae1534123a1b0f807107156e7517f92916e8c7
unknown <danjwuest@gmail.com>
2024-01-19 21:51:42 -0800

    Update app.js writeDate

--- app.js
+++ app.js
@@ -11,5 +11,19 @@ function writeDate() {
 }
 
 writeDate();
+function writeDate() {
+    const today = new Date();
+    const formatter = new Intl.DateTimeFormat('en-us', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+    });
+    const dateEl = document.getElementById('date');
+    dateEl.textContent = formatter.format(today);
+    }
+    writeDate();
 
 console.log('Application ready');
