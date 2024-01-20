4cb8172347a3135f389947e6860834b9e412fed9
unknown <danjwuest@gmail.com>
2024-01-19 21:52:35 -0800

    Update actually saved app.js writeDate

--- app.js
+++ app.js
@@ -4,19 +4,6 @@ function writeDate() {
     weekday: 'long',
     month: 'long',
     day: 'numeric',
-    hour12: true
-    });
-    const dateEl = document.getElementById('date');
-    dateEl.textContent = formatter.format(today);
-}
-
-writeDate();
-function writeDate() {
-    const today = new Date();
-    const formatter = new Intl.DateTimeFormat('en-us', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
     hour: 'numeric',
     minute: 'numeric',
     hour12: true
